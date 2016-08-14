4a118b7c55aeb00f17e45c2f5fda3aeb8a7d6328
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-14 17:14:42 +0100

    disable some docker container

--- puppet/environments/db/manifests/default.pp
+++ puppet/environments/db/manifests/default.pp
@@ -2,20 +2,20 @@ class { 'docker':
   version => '1.12.0'
 }
 
-docker::image { 'centos': image_tag => 7 }
+#docker::image { 'centos': image_tag => 7 }
+##docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker
+#docker::run { 'docker-ui':
+#  privileged => true,
+#  image => 'centos',
+#  volumes => "/var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker",
+#  ports => '9000:9000'
+#}
 
 docker::image { 'cassandra': image_tag => 3.7 }
 docker::run { 'cassandra-1':
   image => 'cassandra',
   tag => 3.7
 }
-
-#docker run --name some-cassandra2 -d -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' some-cassandra)" cassandra:tag
-#docker::run { 'cassandra-2':
-#  image => 'cassandra',
-#  env => "CASSANDRA_SEEDS=\"$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cassandra-1)\"",
-#  tag => 3.7
-#}
 
 #docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:tag
 docker::run { 'cassandra-2':
@@ -26,10 +26,3 @@ docker::run { 'cassandra-2':
 
 
 
-#docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker
-docker::run { 'docker-ui':
-  privileged => true,
-  image => 'centos',
-  volumes => "/var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker",
-  ports => '9000:9000'
-}
--- puppet/environments/web/manifests/default.pp
+++ puppet/environments/web/manifests/default.pp
@@ -1,11 +1,10 @@
-#exec { "echo 'hello world!!!!'": path => "/usr/bin" }
-class { 'docker':
-    version => '1.12.0'
-}
+#class { 'docker':
+#    version => '1.12.0'
+#}
 
-docker::image { 'centos': image_tag => 7 }
-
-docker::run { 'helloworld': 
-   image => 'centos',
-   command => '/bin/sh -c "echo hello world"'
-}
+#docker::image { 'centos': image_tag => 7 }
+#
+#docker::run { 'helloworld':
+#   image => 'centos',
+#   command => '/bin/sh -c "echo hello world"'
+#}

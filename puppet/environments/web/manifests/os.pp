143d158ace2365870153558f0316ac423983bfb6
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-14 17:53:43 +0100

    first try of motd. failed

--- puppet/environments/web/manifests/os.pp
+++ puppet/environments/web/manifests/os.pp
@@ -1,6 +1,28 @@
 ### MOTD
 class {'linux::base::motd' :
-  content => '../motd.esb',
+  #template => '../motd.erb'
+  content =>
+    'boxes -d shell -p a1l2 \
+<(banner "server"; \
+printf "Hostname: %s\nDate    : %s\nUptime  :%s\n" \
+"$(hostname -s)" "$(date)" "$(uptime)")
+
+HOSTNAME=`uname -n`
+KERNEL=`uname -r`
+CPU=`uname -p`
+ARCH=`uname -m`
+  # The different colours as variables
+W="\033[01;37m"
+B="\033[01;34m"
+R="\033[01;31m"
+X="\033[00;37m"
+echo "$R#===================================================#"
+echo  "       $W Welcome $B $USER $W to $B $HOSTNAME  "
+echo  "       $R ARCH   $W= $ARCH                     "
+echo  "       $R KERNEL $W= $KERNEL                   "
+echo  "       $R CPU    $W= $CPU                      "
+echo  "$R#==================================================#"
+'
 }
 
 ### timezone

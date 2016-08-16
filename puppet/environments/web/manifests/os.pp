199445be6aafe3b70c067a8415f838ce704647de
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-16 23:29:23 +0100

    setted up motd

--- puppet/environments/web/manifests/os.pp
+++ puppet/environments/web/manifests/os.pp
@@ -1,28 +1,24 @@
 ### MOTD
+$motdTemplateErb ="
+#########################################################
+# Hello, welcome to the '<%= @hostname %>'.
+#########################################################
+# Hostname		: <%= @fqdn %>
+<% if @domain %>
+# Domain		: <%= @domain %>
+<% end %>
+# IP address		: <%= @ipaddress %> / <%= @netmask %>
+# Operating System	: <%= @operatingsystem %>  <%= @operatingsystemrelease %> [kernel ver <%= @kernelversion %> ]
+<% if @is_virtual == 'true' %>
+# Virtual		: <%= @productname %> - <%= @virtual %>
+<% end %>
+# Total CPU / MEM 	: <%= @processorcount %> / <%= @memorysize_mb %> MB
+# SECURITY - SELINUX	: <%= @selinux_config_mode %> / <%= @selinux_current_mode %>
+# TIMEZONE		: <%= @timezone %>
+######################################################
+"
 class {'linux::base::motd' :
-  #template => '../motd.erb'
-  content =>
-    'boxes -d shell -p a1l2 \
-<(banner "server"; \
-printf "Hostname: %s\nDate    : %s\nUptime  :%s\n" \
-"$(hostname -s)" "$(date)" "$(uptime)")
-
-HOSTNAME=`uname -n`
-KERNEL=`uname -r`
-CPU=`uname -p`
-ARCH=`uname -m`
-  # The different colours as variables
-W="\033[01;37m"
-B="\033[01;34m"
-R="\033[01;31m"
-X="\033[00;37m"
-echo "$R#===================================================#"
-echo  "       $W Welcome $B $USER $W to $B $HOSTNAME  "
-echo  "       $R ARCH   $W= $ARCH                     "
-echo  "       $R KERNEL $W= $KERNEL                   "
-echo  "       $R CPU    $W= $CPU                      "
-echo  "$R#==================================================#"
-'
+  content => inline_template($motdTemplateErb)
 }
 
 ### timezone

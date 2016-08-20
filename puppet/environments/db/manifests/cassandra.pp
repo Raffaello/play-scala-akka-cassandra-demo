089f7bac3cb2cbab79449fa5df12254f92bbe809
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-20 16:21:12 +0100

    updated cassandra config

--- puppet/environments/db/manifests/cassandra.pp
+++ puppet/environments/db/manifests/cassandra.pp
@@ -15,7 +15,13 @@ class { 'cassandra::datastax_repo':
 	listen_address		=> "${::ipaddress}",
 	seeds			=> "${::ipaddress}",
 	auto_bootstrap		=> false,
-	require => Class['cassandra::datastax_repo', 'java']
+	require => Class['cassandra::datastax_repo', 'java'],
+	concurrent_reads => 8,
+  concurrent_writes => 8,
+  concurrent_counter_writes => 8,
+  file_cache_size_in_mb => 8,
+  memtable_heap_space_in_mb => 32,
+  memtable_offheap_space_in_mb => 24
 }
 #-> class { 'cassandra::schema':
 #  users => { 'cassandra' => { password => 'cassandra'}},
@@ -27,3 +33,20 @@ class { 'cassandra::datastax_repo':
 #}
 #-> class { '::cassandra::opscenter': }
 #
+
+class { 'cassandra::env':
+  file_lines => {
+    'MAX_HEAP_SIZE' => {
+      line  => 'MAX_HEAP_SIZE="64M"',
+      match => '#MAX_HEAP_SIZE="4G"',
+    },
+    'HEAP_NEWSIZE'  => {
+      line  => "HEAP_NEWSIZE='32M'",
+      match => '#HEAP_NEWSIZE="800M"',
+    },
+    'JVM_OPTS server.hostname' => {
+      line => 'JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=127.0.0.1',
+      match => 'JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=<public name>'
+    }
+  }
+}
--- puppet/environments/db/manifests/default.pp
+++ puppet/environments/db/manifests/default.pp
@@ -1,6 +1,6 @@
-class { 'docker':
-  version => '1.12.0'
-}
+#class { 'docker':
+#  version => '1.12.0'
+#}
 
 #docker::image { 'centos': image_tag => 7 }
 ##docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker
@@ -11,18 +11,18 @@ class { 'docker':
 #  ports => '9000:9000'
 #}
 
-docker::image { 'cassandra': image_tag => 3.7 }
-docker::run { 'cassandra-1':
-  image => 'cassandra',
-  tag => 3.7
-}
-
-#docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:tag
-docker::run { 'cassandra-2':
-  image => 'cassandra',
-  links => 'cassandata-1:cassandra',
-  tag => 3.7
-}
+#docker::image { 'cassandra': image_tag => 3.7 }
+#docker::run { 'cassandra-1':
+#  image => 'cassandra',
+#  tag => 3.7
+#}
+#
+##docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:tag
+#docker::run { 'cassandra-2':
+#  image => 'cassandra',
+#  links => 'cassandata-1:cassandra',
+#  tag => 3.7
+#}
 
 
 

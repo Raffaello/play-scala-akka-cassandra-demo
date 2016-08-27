3e5a64a2558d234f439c178bcfda35d2bde89bf9
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-27 19:30:29 +0100

    docker cassandra 2 nodes

--- puppet/environments/db/manifests/docker.pp
+++ puppet/environments/db/manifests/docker.pp
@@ -20,23 +20,24 @@ docker::run { 'docker-ui':
 
 # docker run --name some-cassandra -d cassandra:tag
 docker::image { 'cassandra': image_tag => 3.7 }
-#docker::run { 'cassandra-1':
-#  image => 'cassandra:3.7',
-#  name => 'cassandra-1',
-#  require => Docker::Image[ 'cassandra'],
-#  remove_container_on_stop  => false
-##  tag => 3.7
-#}
+
+docker::run { 'cassandra-1':
+  image => 'cassandra:3.7',
+  name => 'cassandra-1',
+  require => Docker::Image[ 'cassandra'],
+  remove_container_on_stop  => false
+#  tag => 3.7
+}
 
 ##docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:tag
-#-> docker::run { 'cassandra-2':
-#  image => 'cassandra:3.7',
-#  links => 'cassandata-1:cassandra',
-#  name => 'cassandra-2',
-#  require => Docker::Image[ 'cassandra'],
-#  remove_container_on_stop  => false
-##  tag => 'cassandra:3.7'
-#}
+-> docker::run { 'cassandra-2':
+  image => 'cassandra:3.7',
+  links => 'cassandata-1',
+  name => 'cassandra-2',
+  require => Docker::Image[ 'cassandra'],
+  remove_container_on_stop  => false
+#  tag => 'cassandra:3.7'
+}
 
 # docker run --name some-cassandra2 -d -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' some-cassandra)" cassandra:tag
 # docker run --name some-cassandra2 -d --link some-cassandra:cassandra cassandra:tag

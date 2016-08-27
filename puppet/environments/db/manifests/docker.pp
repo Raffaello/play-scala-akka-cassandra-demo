ba7c55f6c9b39c19ea15445a64d8ee60c7c9d156
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-27 18:38:14 +0100

    correct docket-ui container

--- puppet/environments/db/manifests/docker.pp
+++ puppet/environments/db/manifests/docker.pp
@@ -11,7 +11,7 @@ docker::image { 'uifd/ui-for-docker':
 docker::run { 'docker-ui':
   privileged => true,
   image => 'uifd/ui-for-docker',
-  volumes => "/var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker",
+  volumes => "/var/run/docker.sock:/var/run/docker.sock",
   ports => '9000:9000',
   name => 'docker-ui',
   require => Docker::Image[ 'uifd/ui-for-docker' ],

26f655327f749f002b6194311a19d20484056ed9
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-27 19:19:20 +0100

    java db-vm updated

--- puppet/environments/db/manifests/java.pp
+++ puppet/environments/db/manifests/java.pp
@@ -1,9 +1,18 @@
+package {'java-1.7.0-openjdk':
+  ensure => absent,
+  before => Class['java']
+}
+package {'java-1.8.0-openjdk':
+  ensure => absent,
+  before => Class['java']
+}
+->
 class { 'java':
 #  distribution => 'oracle-jdk',
   java_alternative => 'java',
   java_alternative_path => '/usr/java/jdk1.8.0_51/jre/bin/java'
-
 }
+
 java::oracle { 'jdk8' :
   ensure  => 'present',
   version => '8',
@@ -15,4 +24,6 @@ package { 'jna':
   ensure => present,
 #  before => Class['cassandra']
 }
+
+
 

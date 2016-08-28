edd00c071c6a11a4b0aad79ad42e3598d1d20b24
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-28 15:24:26 +0100

    first swarm try

--- puppet/environments/db/manifests/docker_swarm.pp
+++ puppet/environments/db/manifests/docker_swarm.pp
@@ -1,22 +1,32 @@
-#class { 'consul':
-#  config_hash => {
-#    'bootstrap_expect' => 1,
-#    'data_dir'         => '/opt/consul',
-#    'datacenter'       => '0.0.0.0',
-#    'log_level'        => 'INFO',
-#    'node_name'        => 'consul-server',
-#    'bind_addr'        => "${::ipaddress_eth1}"
-#    'server'           => true
-#  }
-#}
-
 #class dockerSwarm {
 #  docker::image { 'swarm': image_tag => latest }
 #  ::docker::run { 'swarm':
 #    image   => 'swarm',
-#    command => "join --addr=${::ipaddress_eth1}:2375 consul://${::ipaddress_eth1}:8500/swarm_nodes"
+#    command => "join --addr=${::ipaddress}:2375 consul://${::ipaddress}:8500/swarm_nodes"
 #  }
 #}
+
+docker::image { 'swarm': image_tag => latest }
+::docker::run { 'swarm':
+  image   => 'swarm',
+  command => "join --addr=${::ipaddress}:2375 consul://${::ipaddress}:8500/swarm_nodes"
+}
+
+::docker::run { 'swarm-manager':
+  image => 'swarm',
+  ports => '3000:2375',
+  command => "manage consul://${ipaddress}:8500/swarm_nodes",
+  require => docker::run['swarm']
+}
+
+exec { 'consul join db.dev':
+  path      => '/usr/local/bin/',
+  require   => Class['consul'],
+  before    => Class['docker'],
+  tries     => 10,
+  try_sleep => 1,
+}
+
 #
 #node 'swarm-1' {
 #  include dockerSwarm

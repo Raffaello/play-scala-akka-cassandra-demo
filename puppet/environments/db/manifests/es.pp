b9b0ecdb83195eebdf95c0cd1ecdf2892eed4efa
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-29 23:03:31 +0100

    es config puppet

--- puppet/environments/db/manifests/es.pp
+++ puppet/environments/db/manifests/es.pp
@@ -1,22 +1,34 @@
 class esNode {
+
+}
+
+node /^es-\d+$/ {
+  include defaultNode
+  include javaNode
+  include esNode
+
+  package { 'wget':
+    ensure => present
+  }
+
   $config_hash = {
     'ES_HEAP_SIZE' => '128M',
     'ES_MAX_HEAP_SIZE' => '128M'
   }
-}
-
-node /^es-\d+$/ {
-  include javaNode
-  include esNode
 
   class { 'elasticsearch':
     java_install      => false,
     package_url => 'https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.5/elasticsearch-2.3.5.rpm',
-    manage_repo       => true,
-    repo_version      => '2.3.5',
+#    manage_repo       => false,
+#    repo_version      => '2.3.5',
     restart_on_change => true,
     autoupgrade       => true,
-
+    require => Package['wget'],
+    init_defaults => $config_hash,
+    config => {
+      'cluster.name' => 'TitanDB Index',
+      'network.host' => $::ipaddress
+    }
   }
 
   elasticsearch::instance { $hostname: }

117467c97240d173c4118dee4054fc168e334b84
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-29 22:37:42 +0100

    typo. puppetversion 4.5.2

--- puppet/environments/db/manifests/es.pp
+++ puppet/environments/db/manifests/es.pp
@@ -11,8 +11,8 @@ node /^es-\d+$/ {
 
   class { 'elasticsearch':
     java_install      => false,
-    package_url => 'https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.5/elasticsearch-2.3.5.rpm'
-    manage_repo       => false,
+    package_url => 'https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.5/elasticsearch-2.3.5.rpm',
+    manage_repo       => true,
     repo_version      => '2.3.5',
     restart_on_change => true,
     autoupgrade       => true,

8d6c04555833a31f1f919af446a1654697adb3ea
Raffaello Bertini <raffaellobertini@gmail.com>
2016-08-29 21:42:46 +0100

    es manifest

---
+++ puppet/environments/db/manifests/es.pp
@@ -0,0 +1,26 @@
+class esNode {
+  $config_hash = {
+    'ES_HEAP_SIZE' => '128M',
+    'ES_MAX_HEAP_SIZE' => '128M'
+  }
+}
+
+node '^/es-\d+$/' {
+  include javaNode
+  include esNode
+
+  class { 'elasticsearch':
+    java_install      => false,
+    manage_repo       => true,
+    repo_version      => '2.3.5',
+    restart_on_change => true,
+    autoupgrade       => true,
+
+  }
+
+  elasticsearch::instance { $hostname: }
+
+  elasticsearch::plugin { 'lmenezes/elasticsearch-kopf':
+    instances => $hostname
+  }
+}

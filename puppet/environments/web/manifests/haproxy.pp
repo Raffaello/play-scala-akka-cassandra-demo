69834cb536ac0632e6b6c3dfa80545ba39672a28
Raffaello Bertini <raffaellobertini@gmail.com>
2016-10-09 19:00:53 +0100

    fixed haproxy manifest

--- puppet/environments/web/manifests/haproxy.pp
+++ puppet/environments/web/manifests/haproxy.pp
@@ -1,7 +1,9 @@
 class haproxyNode {
   class { 'haproxy':
+    service_ensure => true,
     global_options => {
       'log' => '127.0.0.1',
+      'maxconn' => 10
     },
 #    defaults_options => {
 #      'mode' => 'http'
@@ -11,17 +13,49 @@ class haproxyNode {
 
   haproxy::listen { 'web-haproxy':
     ipaddress => $::ipaddress_eth1,
-    ports     => ['80'],
-    bind => $::ipaddress_eth1,
+    ports     => '81',
     mode => 'http',
-    options => ['httplog','dontlognull']
+    options => {
+      'option' => ['httplog','dontlognull'],
+      'balance' => 'roundrobin'
+    }
   }
 
-  haproxy::balancermember { $fqdn:
+  haproxy::balancermember { 'playwebserver01':
     listening_service => 'web-haproxy',
     server_names      => $::hostname,
     ipaddresses       => $::ipaddress_eth1,
-    ports             => '8140',
+    ports             => '8080',
     options           => 'check'
+  }
+
+  haproxy::balancermember { 'playwebserver02':
+    listening_service => 'web-haproxy',
+    server_names      => $::hostname,
+    ipaddresses       => $::ipaddress_eth1,
+    ports             => '9080',
+    options           => 'check'
+  }
+
+  haproxy::frontend { 'http-in':
+    ipaddress => $::ipaddress_eth1,
+    ports => '80',
+    mode => 'http',
+#    bind_options => 'accept-proxy',
+    options => {
+      'default_backend' => 'play_webserver',
+      'timeout client' => '10s',
+      'option' => ['httplog', 'dontlognull']
+    }
+  }
+
+  haproxy::backend { 'play_webserver':
+    collect_exported => false,
+    options => {
+      'option'  => [
+        'httplog'
+      ],
+      'balance' => 'roundrobin'
+    }
   }
 }

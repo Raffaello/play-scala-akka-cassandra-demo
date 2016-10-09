dc872e78c63e05a37cb0f199839909fd1fc1baff
Raffaello Bertini <raffaellobertini@gmail.com>
2016-10-09 21:39:14 +0100

    scala phantom first config...

---
+++ app/models/cassandra/connection.scala
@@ -0,0 +1,29 @@
+package models.cassandra
+
+import com.datastax.driver.core.SocketOptions
+import com.typesafe.config.ConfigFactory
+import com.websudos.phantom.dsl._
+import scala.collection.JavaConversions._
+
+
+object Defaults {
+  private val cassandraConfig = ConfigFactory.load.getConfig("cassandra")
+  val port = cassandraConfig.getInt("port")
+  val hosts = cassandraConfig.getStringList("hosts")
+  val keyspace = cassandraConfig.getString("keyspace")
+
+  val Connector = ContactPoints(hosts, port)
+      .withClusterBuilder(_.withSocketOptions(new SocketOptions().setConnectTimeoutMillis(10000)))
+      .keySpace(keyspace)
+}
+
+class AppDatabase(val keyspace: KeySpaceDef) extends Database(keyspace)
+{
+
+}
+
+object AppDatabase extends AppDatabase(Defaults.Connector)
+
+//trait AppDatabaseProvider extends DatabaseProvider {
+//  override val database: AppDatabase = AppDatabase
+//}
--- build.sbt
+++ build.sbt
@@ -7,10 +7,20 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  jdbc,
+//  jdbc,
   cache,
   ws,
+  "com.websudos"  %% "phantom-dsl" % "1.29.3",
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+)
+
+//resolvers += Resolver.bintrayRepo("websudos", "oss-releases")
+resolvers ++= Seq(
+  "Java.net Maven2 Repository"       at "http://download.java.net/maven/2/",
+  "Twitter Repository"               at "http://maven.twttr.com",
+//  Resolver.typesafeRepo("releases"),
+  Resolver.sonatypeRepo("releases"),
+  Resolver.bintrayRepo("websudos", "oss-releases")
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
